5317b04036b54c00391dca1c83bc8e22470ba128
Ian426 <seoyeon8983@naver.com>
2024-02-01 16:37:58 +0900

    hello world

--- ch01/src/ch01/Hello.java
+++ ch01/src/ch01/Hello.java
@@ -5,6 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("안녕");
+		String hello = "안녕 세계!";
+		System.out.println(hello);
 	}
 
 }
